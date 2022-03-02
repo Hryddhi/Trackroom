d1c7fcca6efdb1f01e3a847e2d89be27c886a8cf
Hryddhi <hryddhimirza@gmail.com>
2022-03-02 16:19:45 +0600

    Android Hompage

--- TrackroomAndroid/app/src/main/java/com/hryddhi/trackroomandroid/MainActivity.java
+++ TrackroomAndroid/app/src/main/java/com/hryddhi/trackroomandroid/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends BaseDataActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_trackroom);
         //isLoggedIn();
 
        /* btn_login = (Button) findViewById(R.id.btn_login);
