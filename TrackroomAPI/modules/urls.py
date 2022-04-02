380bf446e3d75548e0157bbd0a3feae31bd3d597
Umme_Hani <umme.hani5@northsouth.edu>
2022-04-02 19:37:27 +0600

    Correction

--- TrackroomAPI/modules/urls.py
+++ TrackroomAPI/modules/urls.py
@@ -8,7 +8,7 @@ from .views import ModuleViewset
 app_name = "modules"
 
 router = SimpleRouter()
-router.register(r'module/<pk>', ModuleViewset, basename='module')
+router.register(r'module', ModuleViewset, basename='module')
 
 urlpatterns = [
     path('api/', include((router.urls, 'module'))),
