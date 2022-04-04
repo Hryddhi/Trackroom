631cdd38041037b02f39e62cd6e507a962d06374
Umme_Hani <umme.hani5@northsouth.edu>
2022-04-04 12:50:24 +0600

    Correction

--- TrackroomAPI/classrooms/permissions.py
+++ TrackroomAPI/classrooms/permissions.py
@@ -3,9 +3,11 @@ from .models import Classroom
 
 
 class ClassroomViewPermission(BasePermission):
-    message = {"detail": "User does not have access to this classroom"}
+    type = ""
+    message = {"detail": f"User does not have {type} access to this classroom"}
 
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return obj.has_this_member(request.user) if isinstance(obj, Classroom) else obj.classroom.has_this_member(request.user)
+        type = "write"
         return obj.has_this_creator(request.user) if isinstance(obj, Classroom) else obj.classroom.has_this_subscriber(request.user)
